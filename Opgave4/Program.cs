605be3905c013744447c0e31b778c6796e53a385
mikth <mikth@THOMSEN-PC>
2025-03-10 23:29:18 +0100

    Add project files.

---
+++ Opgave4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Opgave4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start en TCP-lytter på port 42069
+            TcpListener listener = new TcpListener(IPAddress.Any, 42069);
+            listener.Start();
+            Console.WriteLine("Server startet på port 42069");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Thread t = new Thread(() => TCPServer.HandleClient(client));
+                t.Start();
+            }
+        }
+    }
+}
+
---
+++ Opgave4/TCPServer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave4
+{
+    class TCPServer
+    {
+    public static void HandleClient(TcpClient client)
+        {
+            using (client)
+            {
+                NetworkStream ns = client.GetStream();
+                StreamReader reader = new StreamReader(ns);
+                StreamWriter writer = new StreamWriter(ns) { AutoFlush = true };
+                Random rnd = new Random();
+
+                while (true)
+                {
+                    // 1) Læs kommando
+                    string command = reader.ReadLine();
+                    if (string.IsNullOrWhiteSpace(command)) break;
+
+                    // 2) Svar med "Input numbers"
+                    writer.WriteLine("Input numbers");
+
+                    // 3) Læs 2 tal adskilt af mellemrum
+                    string numbersLine = reader.ReadLine();
+                    if (string.IsNullOrWhiteSpace(numbersLine)) break;
+                    string[] numbers = numbersLine.Split();
+
+                    int n1 = int.Parse(numbers[0]);
+                    int n2 = int.Parse(numbers[1]);
+
+                    // 4) Udfør handling baseret på kommandoen, og skriv resultat
+                    switch (command)
+                    {
+                        case "Random":
+                            writer.WriteLine(rnd.Next(n1, n2 + 1));
+                            break;
+                        case "Add":
+                            writer.WriteLine(n1 + n2);
+                            break;
+                        case "Subtract":
+                            writer.WriteLine(n1 - n2);
+                            break;
+                        default:
+                            // Ukendt kommando, luk forbindelsen
+                            return;
+                    }
+                }
+            }
+        }
+    }
+}
